7bd568ee498d339daca64f8fb49c824fa74e6a9e
Tomas Barrozo <tomy0898@gmail.com>
2023-09-05 01:44:25 -0300

    Avisos y dinamismo

--- main.js
+++ main.js
@@ -2,37 +2,52 @@ const botonReservar = document.getElementById('reservar');
 const listaReservas = document.getElementById('listaReservas');
 const nombreInput = document.getElementById('nombre');
 const fechaInput = document.getElementById('fecha');
+const habitacionSelect = document.getElementById('habitacion');
+const mensajeError = document.getElementById('mensajeError'); // Referencia al elemento de mensaje de error
 
-// Reservas
+// Función para validar el nombre (solo caracteres alfabéticos)
+function esNombreValido(nombre) {
+    return /^[a-zA-Z]+$/.test(nombre);
+}
+
+// Manejar el evento de reserva
 botonReservar.addEventListener('click', function() {
     const nombre = nombreInput.value;
     const fecha = fechaInput.value;
+    const habitacion = habitacionSelect.value;
 
-    if (nombre && fecha) {
-        const reserva = { nombre, fecha };
+    if (nombre && fecha && esNombreValido(nombre)) {
+        const reserva = { nombre, fecha, habitacion };
 
-        // Obtener las reservas o crear un array vacío si no hay ninguna
+        // Obtener las reservas actuales o crear un array vacío si no hay ninguna
         const reservas = JSON.parse(localStorage.getItem('reservas')) || [];
         reservas.push(reserva);
 
         localStorage.setItem('reservas', JSON.stringify(reservas));
 
         mostrarReservas();
+        mensajeError.textContent = ''; // Limpiar mensaje de error
+    } else if (!nombre) {
+        mensajeError.textContent = 'Por favor, complete el campo Nombre.';
+    } else if (!esNombreValido(nombre)) {
+        mensajeError.textContent = 'El nombre debe contener solo caracteres alfabéticos.';
+    } else {
+        mensajeError.textContent = 'Por favor, complete todos los campos.';
     }
 });
 
-// Mostrar reservas
+// Función para mostrar todas las reservas
 function mostrarReservas() {
     const reservas = JSON.parse(localStorage.getItem('reservas')) || [];
     let html = '';
 
     reservas.forEach((reserva, index) => {
-        html += `<li>${reserva.nombre} - ${reserva.fecha} <button data-index="${index}" class="eliminar">Eliminar</button></li>`;
+        html += `<li>${reserva.nombre} - ${reserva.fecha} - Habitación ${reserva.habitacion} <button data-index="${index}" class="eliminar">Eliminar</button></li>`;
     });
 
     listaReservas.innerHTML = html;
 
-    // Proceso de eliminacion de reservas.
+    // Manejar el evento de eliminar reserva
     const botonesEliminar = document.querySelectorAll('.eliminar');
     botonesEliminar.forEach((boton) => {
         boton.addEventListener('click', function() {
